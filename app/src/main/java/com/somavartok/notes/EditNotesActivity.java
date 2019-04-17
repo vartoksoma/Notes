bc0be7c40d5ce692559ee764c352f95a995f88f8
Vartok Soma <vartok.soma@gmail.com>
2019-04-17 12:46:53 +0200

    add note, delete note. stored in ArrayList.

--- app/src/main/java/com/somavartok/notes/EditNotesActivity.java
+++ app/src/main/java/com/somavartok/notes/EditNotesActivity.java
@@ -3,9 +3,13 @@ package com.somavartok.notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class EditNotesActivity extends AppCompatActivity {
+
+    int noteId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,34 @@ public class EditNotesActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.editText);
 
         Intent intent = getIntent();
-        int noteId = intent.getIntExtra("noteId", -1);
+        noteId = intent.getIntExtra("noteId", -1);
 
         if(noteId != -1){
             editText.setText(MainActivity.notes.get(noteId));
+        }else{
+
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size()-1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+
         }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.notes.set(noteId, String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/somavartok/notes/MainActivity.java
+++ app/src/main/java/com/somavartok/notes/MainActivity.java
@@ -1,8 +1,14 @@
 package com.somavartok.notes;
 
+import android.app.Application;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,6 +19,27 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.add_note){
+            Intent intent = new Intent(getApplicationContext(), EditNotesActivity.class);
+
+            startActivity(intent);
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         notes.add("Example note");
         notes.add("loooool");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, notes);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -32,6 +59,28 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), EditNotesActivity.class);
                 intent.putExtra("noteId", i);
                 startActivity(intent);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+                return true;
             }
         });
     }
